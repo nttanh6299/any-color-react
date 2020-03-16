c4666c12886ea07aed13d4cb7b862748d7af7926
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-03-16 11:23:15 +0700

    add button component

---
+++ src/components/button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  children: PropTypes.node,
+  prefix: PropTypes.node,
+  suffix: PropTypes.node,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  style: PropTypes.object
+};
+
+const Button = ({ children, prefix, suffix, onClick, className, style }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ style }}
+      className={`button ${className}`}
+    >
+      {prefix}
+      {children}
+      {suffix}
+    </button>
+  );
+};
+
+Button.propTypes = propTypes;
+
+export default Button;
--- src/components/colors.js
+++ src/components/colors.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Background from './background';
+import Button from './button';
 
 const propTypes = {
   color: PropTypes.string,
@@ -33,18 +34,27 @@ const Colors = ({
         />
         <div className="colors__value">{color}</div>
         <div className="colors__actions">
-          <button
+          <Button
             onClick={onGenerate}
             className="colors__action colors__action--generate"
+            prefix={<i className="icon material-icons">refresh</i>}
           >
-            <i className="icon material-icons">refresh</i> Generate
-          </button>
-          <button onClick={prevColor} className="colors__action">
-            <i className="icon material-icons">arrow_left</i> Back
-          </button>
-          <button onClick={nextColor} className="colors__action">
-            Next <i className="icon material-icons">arrow_right</i>
-          </button>
+            Generate
+          </Button>
+          <Button
+            onClick={prevColor}
+            className="colors__action"
+            prefix={<i className="icon material-icons">arrow_left</i>}
+          >
+            Back
+          </Button>
+          <Button
+            onClick={nextColor}
+            className="colors__action"
+            suffix={<i className="icon material-icons">arrow_right</i>}
+          >
+            Next
+          </Button>
         </div>
       </div>
     </div>
