e4a4b1af0d86f1eeebb8f1c49942aa2a9c035a60
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-04-09 22:50:07 +0700

    move router to index file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import configureStore from './store/configureStore';
 
 const Main = () => (
   <Provider store={configureStore()}>
-    <App />
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </Provider>
 );
 
